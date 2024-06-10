3060ac332054f749a69d6c6c9729dabe0a6f9da8
RifqiAuliaRochman <rifqi6882@gmail.com>
2024-06-10 10:42:39 +0700

    update spring

---
+++ dutamedia-spring/src/main/java/dutamedia/springboot/controller/UserController.java
@@ -0,0 +1,48 @@
+package dutamedia.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;  
+import org.springframework.web.bind.annotation.*;
+
+import dutamedia.springboot.entity.User;
+import dutamedia.springboot.model.RegisterUserRequest;
+import dutamedia.springboot.model.UpdateUserRequest;
+import dutamedia.springboot.model.UserResponse;
+import dutamedia.springboot.model.WebResponse;
+import dutamedia.springboot.service.UserService;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(
+            path = "/api/users",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> register(@RequestBody RegisterUserRequest request) {
+        userService.register(request);
+        return WebResponse.<String>builder().data("OK").build();
+    }
+
+    @GetMapping(
+            path = "/api/users/current",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<UserResponse> get(User user) {
+        UserResponse userResponse = userService.get(user);
+        return WebResponse.<UserResponse>builder().data(userResponse).build();
+    }
+
+    @PatchMapping(
+            path = "/api/users/current",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<UserResponse> update(User user, @RequestBody UpdateUserRequest request) {
+        UserResponse userResponse = userService.update(user, request);
+        return WebResponse.<UserResponse>builder().data(userResponse).build();
+    }
+}
---
+++ dutamedia-spring/src/test/java/dutamedia/springboot/DutamediaSpringbootTest.java
@@ -0,0 +1,13 @@
+package dutamedia.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutamediaSpringbootTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
